c02870160fb4f5b0c79024b14f8831dcf73acf70
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-05-20 20:11:23 -0700

    testing!

--- hw1unittest.py
+++ hw1unittest.py
@@ -4,6 +4,12 @@ import ekkachai_ittihrit_hw1
 class testLeap(unittest.TestCase):
     def testZero(self):
         self.assertEqual(ekkachai_ittihrit_hw1.leap(0), "Leap year!")
+    def testWorkingLeapYear(self):
+        self.assertEqual(ekkachai_ittihrit_hw1.leap(2000), "Leap year!")
+    def testNeg(self):
+        self.assertEqual(ekkachai_ittihrit_hw1.leap(-1900), "Invalid input")
+    def testNonLeapYear(self):
+        self.assertEqual(ekkachai_ittihrit_hw1.leap(2001), "Not a leap year!")
 
 
 
