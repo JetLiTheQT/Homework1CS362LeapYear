688390900ae8cfe07b5e404c936138a36b16b22d
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-05-20 20:03:47 -0700

    new commit with unit test file

---
+++ hw1unittest.py
@@ -0,0 +1,12 @@
+import unittest
+import ekkachai_ittihrit_hw1
+
+class testLeap(unittest.TestCase):
+    def testZero(self):
+        self.assertEqual(ekkachai_ittihrit_hw1.leap(0), "leap year!")
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
